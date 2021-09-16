5f244e018868496efbe0325fd61669480061900d
Mateusz Sipiński <mateuszsipinski@gmail.com>
2021-09-16 08:55:53 +0200

    Add TypeScript definition (#6)

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+/**
+The user's default shell
+*/
+declare const defaultShell: string;
+export default defaultShell;
---
+++ index.test-d.ts
@@ -0,0 +1,4 @@
+import {expectType} from 'tsd';
+import defaultShell from './index.js';
+
+expectType<string>(defaultShell);
